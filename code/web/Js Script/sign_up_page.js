9e2d60f1f287c5a2ad2859e54f76a99ce91a1bcf
SandalsPhone <jason.jevando@gmail.com>
2022-12-03 20:27:52 +0700

    Edited sign_up_page.html to have a form and misc

---
+++ code/web/Js Script/sign_up_page.js
@@ -0,0 +1,10 @@
+//action listener untuk button gender(m/f), saat di klik akan membuat json untuk di parse oleh sign_up_submit.php
+let male_button = document.querySelector('.gender_male_button')
+let female_button = document.querySelector('.gender_female_button')
+
+male_button.addEventListener('click', function(){
+})
+
+female_button.addEventListener('click', function(){
+    
+})
