c25628141ceb0db8323bbf4b01f92569982f3bdb
Prapta <116350366+Prapta@users.noreply.github.com>
2022-12-04 14:57:24 +0700

    Add files via upload

---
+++ code/web/Js Script/myprofile_page.js
@@ -0,0 +1,9 @@
+//buat popup signin segala macem
+function showingDiv(){
+    var x = document.getElementById("btn");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
