2ecdf8fae6ff205330c195f21b8c7b8e2a10b894
SandalsPhone <jason.jevando@gmail.com>
2022-12-04 17:54:42 +0700

    Finish sign_up_submit.php functions & update db

--- code/data/init.sql
+++ code/data/init.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db:3306
--- Generation Time: Dec 04, 2022 at 04:12 AM
+-- Generation Time: Dec 04, 2022 at 10:54 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.19
 
@@ -32,13 +32,20 @@ USE `akun`;
 CREATE TABLE `akun` (
   `first_name_akun` varchar(50) NOT NULL,
   `last_name_akun` varchar(50) NOT NULL,
-  `username_akun` varchar(50) NOT NULL,
   `email_akun` varchar(50) NOT NULL,
   `address_akun` varchar(100) NOT NULL,
-  `no_hp_akun` int NOT NULL,
+  `no_hp_akun` varchar(20) NOT NULL,
   `password_akun` varchar(100) NOT NULL,
   `gender_akun` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `akun`
+--
+
+INSERT INTO `akun` (`first_name_akun`, `last_name_akun`, `email_akun`, `address_akun`, `no_hp_akun`, `password_akun`, `gender_akun`) VALUES
+('Contoh', 'Example', 'aes101@gmail.com', 'chicago', '81222222222', 'aaaaa', 'Male'),
+('Contoh', 'Example', 'aes11@gmail.com', 'chicago', '81222222222', 'aaaaa', 'Male');
 --
 -- Database: `book_a_playdate`
 --
@@ -62,6 +69,16 @@ CREATE TABLE `book_a_playdate` (
   `address_book_playdate_2` varchar(100) NOT NULL,
   `address_book_playdate_3` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `book_a_playdate`
+--
+
+INSERT INTO `book_a_playdate` (`gambar_book_a_playdate`, `tempat_book_playdate`, `available_date_book_playdate_1`, `available_date_book_playdate_2`, `available_date_book_playdate_3`, `area_book_playdate`, `address_book_playdate_1`, `address_book_playdate_2`, `address_book_playdate_3`) VALUES
+('pic/image 18.png', 'CENTRAL PARK', '2022-11-23', '2022-11-25', '2022-11-27', 'Park, Lantai 1', 'Letjen S. Parman St No.kav.28, South Tanjung Duren,', 'Grogol petamburan, West Jakarta City', ''),
+('pic/image 17.png', 'LIVING WORLD', '2022-11-24', '2022-11-26', '2022-11-28', 'Playground, Lantai 1', 'Jalan Alam Sutera Boulevard Kav.21 15325', 'Tangerang Selatan Banten', ''),
+('pic/image 49.png', 'EMPORIUM MALL', '2022-11-23', '2022-11-25', '2022-11-27', 'FoodHall, Lantai 4', 'Jl. Pluit Selatan Raya, Jakarta, Indonesia', '', ''),
+('pic/image 50.png', 'PLUIT VILLAGE', '2022-11-24', '2022-11-26', '2022-11-28', 'Playground, Lantai 2', 'Jalan Pluit Selatan Raya 14440 Penjaringan', 'Jakarta', '');
 --
 -- Database: `learning_kit`
 --
@@ -252,7 +269,7 @@ CREATE TABLE `pma__recent` (
 --
 
 INSERT INTO `pma__recent` (`username`, `tables`) VALUES
-('root', '[{\"db\":\"akun\",\"table\":\"akun\"},{\"db\":\"learning_kit\",\"table\":\"learning_kit\"},{\"db\":\"book_a_playdate\",\"table\":\"book_a_playdate\"},{\"db\":\"service_page\",\"table\":\"service_page\"},{\"db\":\"service\",\"table\":\"service\"},{\"db\":\"review\",\"table\":\"review\"},{\"db\":\"product_page\",\"table\":\"product_page\"},{\"db\":\"product\",\"table\":\"product\"},{\"db\":\"uph\",\"table\":\"tableuph\"}]');
+('root', '[{\"db\":\"akun\",\"table\":\"akun\"},{\"db\":\"book_a_playdate\",\"table\":\"book_a_playdate\"},{\"db\":\"learning_kit\",\"table\":\"learning_kit\"},{\"db\":\"service_page\",\"table\":\"service_page\"},{\"db\":\"service\",\"table\":\"service\"},{\"db\":\"review\",\"table\":\"review\"},{\"db\":\"product_page\",\"table\":\"product_page\"},{\"db\":\"product\",\"table\":\"product\"},{\"db\":\"uph\",\"table\":\"tableuph\"}]');
 
 -- --------------------------------------------------------
 
@@ -359,7 +376,7 @@ CREATE TABLE `pma__userconfig` (
 --
 
 INSERT INTO `pma__userconfig` (`username`, `timevalue`, `config_data`) VALUES
-('root', '2022-12-04 04:05:21', '{\"Console\\/Mode\":\"collapse\"}');
+('root', '2022-12-04 10:53:28', '{\"Console\\/Mode\":\"collapse\"}');
 
 -- --------------------------------------------------------
 
--- code/web/sign_up_submit.php
+++ code/web/sign_up_submit.php
@@ -19,14 +19,69 @@ $password = $_POST['password'];
 $password_retype = $_POST['password_retype'];
 $phone_number = $_POST['phone_number'];
 
+$query = "SELECT `email_akun` FROM `akun`";
+$qr = mysqli_query($conn, $query);
+$email_array = array();
+
+while($row = mysqli_fetch_array($qr)){
+     $email_array[] = $row['email_akun'];
+}
+
+$email_array_size = count($email_array);
+
+//calculate phone number int length, used https://stackoverflow.com/questions/28433798/php-get-length-of-digits-in-a-number as ref
+if($phone_number !== 0){
+    $phone_number_length = strlen((string)$phone_number);
+}
+else{
+    $phone_number_length = 1;
+}
+
+//check if all inpputs are filled
+if(empty($first_name)
+    || empty($last_name)
+    || empty($gender)
+    || empty($email)
+    || empty($address)
+    || empty($password)
+    || empty($password_retype)
+    || empty($phone_number)
+){
+    header('Location: sign_up_page.html');
+    exit();
+}
+//check if name is valid
+else if(!(ctype_alpha($first_name)) || !(ctype_alpha($last_name))){
+    header('Location: sign_up_page.html');
+    exit();
+}
 //check if both password and password_retype are the same
-if($password != $password_retype){
+else if($password != $password_retype){
     header('Location: sign_up_page.html');
     exit();
 }
-else if($phone_number < 7 || $phone_number > 13) {
+//check if phone number is valid
+else if($phone_number_length < 7 || $phone_number_length > 13) {
     header('Location: sign_up_page.html');
     exit();
 }
-//validate phone number
-//on hold, current database is wrong
+//check if email address is valid
+else if(!(filter_var($email, FILTER_VALIDATE_EMAIL))){
+    header('Location: sign_up_page.html');
+    exit();
+}
+else{
+    //check if email is already in database
+    for($i = 0; $i<$email_array_size; $i++){
+        if ($email_array[$i] == $email){
+            header('Location: sign_up_page.html');
+            exit();
+        }
+    }
+    //insert details into db
+    //WARNING: THIS HAS NO SECURITY AT ALL, 100% VUNERABLE TO SQL INJECTION
+    $query = "INSERT INTO `akun` (`first_name_akun`, `last_name_akun`, `email_akun`, `address_akun`, `no_hp_akun`, `password_akun`, `gender_akun`) VALUES ('$first_name' ,'$last_name', '$email', '$address', '$phone_number', '$password', '$gender')";
+    $qr = mysqli_query($conn, $query);
+    header('Location: myprofile_page.html');
+    exit();
+}
